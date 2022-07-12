e2a84e594300c14077ffb6b083c48d24e3687f7a
Александр <107046079+MrDragoRozum@users.noreply.github.com>
2022-07-12 14:22:22 +0300

    Create or35.java

---
+++ Task From Codingbat/Warmup-1/or35.java
@@ -0,0 +1,16 @@
+
+// Return true if the given non-negative number is a multiple of 3 or a multiple of 5. Use the % "mod" operator -- see Introduction to Mod
+
+
+// or35(3) → true
+// or35(10) → true
+// or35(8) → false
+
+// https://codingbat.com/prob/p112564
+
+public boolean or35(int n) {
+  if(n % 5 == 0 || n % 3 == 0) {
+    return true;
+  }
+  return false;
+}
