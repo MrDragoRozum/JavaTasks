d7f1bf733ebb48b0927c165ff07f90e754ece552
Александр <107046079+MrDragoRozum@users.noreply.github.com>
2022-07-12 15:09:05 +0300

    Create startHi.java

---
+++ Task From Codingbat/Warmup-1/startHi.java
@@ -0,0 +1,13 @@
+
+// Given a string, return true if the string starts with "hi" and false otherwise.
+
+
+// startHi("hi there") → true
+// startHi("hi") → true
+// startHi("hello hi") → false
+
+// https://codingbat.com/prob/p191022
+
+public boolean startHi(String str) {
+  return str.matches("^hi.*");
+}
