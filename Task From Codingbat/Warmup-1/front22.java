9a891da2ce44f2640b66cbb99fc1661ed5ec62eb
Александр <107046079+MrDragoRozum@users.noreply.github.com>
2022-07-12 14:45:44 +0300

    Create front22.java

---
+++ Task From Codingbat/Warmup-1/front22.java
@@ -0,0 +1,15 @@
+// Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back, so "kitten" yields"kikittenki".
+//   If the string length is less than 2, use whatever chars are there.
+
+
+// front22("kitten") → "kikittenki"
+// front22("Ha") → "HaHaHa"
+// front22("abc") → "ababcab"
+
+//https://codingbat.com/prob/p183592
+
+public String front22(String str) {
+      int count = Math.min(str.length(), 2);
+		  String tmp = str.substring(0, count);
+		  return tmp + str + tmp;
+}
