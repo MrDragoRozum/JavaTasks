7f2ca0aa0cf45150a8e431011525e68537991baf
Александр <107046079+MrDragoRozum@users.noreply.github.com>
2022-07-12 13:35:56 +0300

    Create front3.java

---
+++ Task From Codingbat/Warmup-1/front3.java
@@ -0,0 +1,25 @@
+
+// Given a string, we'll say that the front is the first 3 chars of the string.
+// If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+
+
+// front3("Java") → "JavJavJav"
+// front3("Chocolate") → "ChoChoCho"
+// front3("abc") → "abcabcabc"
+
+// https://codingbat.com/prob/p136351
+
+public String front3(String str) {
+  String tmp = "";
+        
+        if(str.length() > 3) {
+             for(int i = 0; i < 3; i++) {
+            tmp = str.substring(0, i+1);
+             }
+        } else {
+             for(int i = 0; i < str.length(); i++) {
+            tmp = str.substring(0, i+1);
+             }
+        }
+        return tmp += tmp + tmp;
+}
