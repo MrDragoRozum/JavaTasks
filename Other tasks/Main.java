20130e7fed1622a5152eb417e5d3a32cbfda0012
MrDragoRozum <roozumaleks@gmail.com>
2022-07-22 13:45:32 +0300

    Добавлено решение задачи

---
+++ Other tasks/Main.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Random random = new Random();
+		int number = random.nextInt(6) + 1;
+		System.out.printf("Выпало: %s", number);
+
+	}
+
+}
